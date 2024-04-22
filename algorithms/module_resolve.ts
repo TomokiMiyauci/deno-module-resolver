fb75305a25b7eb53613a62c505f9a175ff26613b
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2024-04-22 21:31:16 +0900

    refactor(node_resolve): split modules

--- algorithms/module_resolve.ts
+++ algorithms/module_resolve.ts
@@ -1,9 +1,10 @@
 import { esmResolve } from "./esm_resolve.ts";
 import { npmResolve } from "./npm_resolve.ts";
 import { jsonResolve } from "./json_resolve.ts";
+import { nodeResolve } from "./node_resolve.ts";
 import { assertedResolve } from "./asserted_resolve.ts";
 import type { Module, Source } from "../deps.ts";
-import { type ResolveOptions, ResolveResult } from "./types.ts";
+import type { ResolveOptions, ResolveResult } from "./types.ts";
 
 export function moduleResolve(
   module: Module,
@@ -11,24 +12,19 @@ export function moduleResolve(
   options: ResolveOptions,
 ): Promise<ResolveResult> | ResolveResult {
   switch (module.kind) {
-    case "esm": {
+    case "esm":
       return esmResolve(module);
-    }
 
-    case "npm": {
+    case "npm":
       return npmResolve(module, source, options);
-    }
 
-    case "node": {
-      return { url: new URL(module.specifier), mediaType: "Unknown" };
-    }
+    case "node":
+      return nodeResolve(module);
 
-    case "asserted": {
+    case "asserted":
       return assertedResolve(module);
-    }
 
-    case "json": {
+    case "json":
       return jsonResolve(module);
-    }
   }
 }
---
+++ algorithms/node_resolve.ts
@@ -0,0 +1,8 @@
+import type { NodeModule } from "../deps.ts";
+import type { ResolveResult } from "./types.ts";
+
+export function nodeResolve(module: NodeModule): ResolveResult {
+  const url = new URL(module.specifier);
+
+  return { url, mediaType: "Unknown" };
+}
